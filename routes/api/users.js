9b05dc89a2c845748a14a7009bdb3f3942daaca7
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-29 11:57:49 +0530

    "Passport jwt authentication and get current user"

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,11 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/Users'); 
+const { authenticate } = require('passport');
 
 // @route    GET api/users/test
 // @desc     Tests users route
@@ -83,7 +85,7 @@ router.post('/login', (req, res) => {
                         (err, token) => {
                             res.json({
                                 success: true,
-                                toekn: 'Bearer' + token
+                                toekn: 'Bearer ' + token
                             })
                     });
                 } else {
@@ -92,4 +94,16 @@ router.post('/login', (req, res) => {
             })
         })
 })
+
+// @route    GET api/users/current
+// @desc     Return current user 
+// @access   Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) =>{
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+})
+
 module.exports = router;
