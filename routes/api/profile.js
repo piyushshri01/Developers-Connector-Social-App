8c8992d5824afa57c59783caec9b7a1451f19205
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-29 23:40:42 +0530

    "Delete User and Profile"

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -259,6 +259,18 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
         .catch(err => res.status(404).json(err))
 });
 
+// @route    DELETE api/profile
+// @desc     Delete user and profile
+// @access   Private
+router.delete('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+            User.findByIdAndRemove({ _id: req.user.id })
+                .then(() => {
+                    res.json({ success: true })
+                })
+        })
+});
 
 
 module.exports = router;
