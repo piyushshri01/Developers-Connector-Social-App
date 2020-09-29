fa738e5438daa0eee9db33f30ffe6626b4622372
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-29 11:22:15 +0530

    "Creating JWT"

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Piyush:zXAJr6FdP4D0pq0M@cluster0.w72po.mongodb.net/devConnector?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Piyush:zXAJr6FdP4D0pq0M@cluster0.w72po.mongodb.net/devConnector?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }
