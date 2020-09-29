ad2eb8eb6601d295007406c37d235ff4157bdf35
piyushshri01 <piyushshrimal94@gmail.com>
2020-09-29 08:40:09 +0530

    "connect mongoose to our  mongodb"

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Piyush:zXAJr6FdP4D0pq0M@cluster0.w72po.mongodb.net/devConnector?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const  mongoose = require('mongoose');
+
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB 
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello')
