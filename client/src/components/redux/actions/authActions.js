dad5e24efd3faffed1f0918dd4480119686a16a5
piyushshri01 <piyushshrimal94@gmail.com>
2020-10-05 20:58:31 +0530

    "Redux login action or set current user"

--- client/src/components/redux/actions/authActions.js
+++ client/src/components/redux/actions/authActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import jwt_decode from 'jwt-decode';
+
+import setAuthToken from '../../../utils/setAuthToken';
+
 // Register User
 export const registerUser = (userData, history) => dispatch => {
     axios
@@ -11,4 +15,36 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
         });
-}; 
+};
+
+// Login - Get User Token
+export const loginuser = (userData) => dispatch => {
+    axios   
+        .post('/pi/users/login', userData)
+        .then(res => {
+            // save to localStorage 
+            const { token } = res.data;
+            // Set token to localstorage
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decode = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decode));
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Set logged in user
+export const setcurrentuser = (decode) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decode
+    }
+}
