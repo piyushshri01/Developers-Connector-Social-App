5a59306f82b0588d00ae45b168d22f8b038ee482
piyushshri01 <piyushshrimal94@gmail.com>
2020-10-08 20:47:40 +0530

    "TextFieldGroupInput Component"

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 // Redux stuff
 import { connect } from 'react-redux';
@@ -58,33 +59,23 @@ class Login extends Component {
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">Sign in to your DevConnector account</p>
               <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input 
-                    type="email" 
-                    className={classnames('form-control form-control-lg',{
-                      'is-invalid': errors.email
-                    })} 
-                    placeholder="Email Address" 
-                    name="email" 
-                    value={email} 
-                    onChange={this.onChange}/>
-                    {errors.email && (
-                      <div className="invalid-feedback">{errors.email}</div>
-                    )}
-                </div>
-                <div className="form-group">
-                  <input 
-                    type="password" 
-                    className={classnames('form-control form-control-lg',{
-                      'is-invalid': errors.password
-                    })}  placeholder="Password" 
-                    name="password" 
-                    value={password} 
-                    onChange={this.onChange}/>
-                    {errors.password && (
-                      <div className="invalid-feedback">{errors.password}</div>
-                    )}
-                </div>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
