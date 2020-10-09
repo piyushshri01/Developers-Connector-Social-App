50dd9b29fb31cfd85bb298585b1db01affe551f8
piyushshri01 <piyushshrimal94@gmail.com>
2020-10-09 11:19:49 +0530

    "CreateProfile compoennt route"

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+
 
 // Private Route
 import PrivateRoute from './components/common/PrivateRoute';
@@ -56,6 +58,9 @@ class App extends Component {
               <Route exact path="/login" component={ Login }/>
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={ CreateProfile }/>
               </Switch>
             </div>
             <Footer/>      
