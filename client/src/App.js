51c09e19f1dec0cce9696264e014853481c3ece9
piyushshri01 <piyushshrimal94@gmail.com>
2020-11-17 17:42:51 +0530

    "Add add-Experience Route and design interface"

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
-
+import AddExperience from './components/add-credentials/AddExperience';
 
 
 // Private Route
@@ -66,6 +66,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/edit-profile" component={ EditProfile }/>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-experience" component={ AddExperience }/>
               </Switch>
             </div>
             <Footer/>      
